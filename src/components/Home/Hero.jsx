a6ff19ee3b472a9615bb30c17ed58a8a64b6370e
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-07-28 15:38:00 +0600

    added title

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,12 @@
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h2 className="hero-title">Fast, Easy, & Accurate</h2>
+      <h3 className="hero-title-two">online translation portal</h3>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Home/Header';
+import Hero from '../../components/Home/Hero';
 import './Home.css';
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Hero />
         </div>
     );
 };
