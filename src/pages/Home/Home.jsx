c5c23930e16ddaffe3f2fc8381d89d92e0902bdf
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-07-28 10:21:24 +0600

    added route & layout

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h4>hello this is a home page.......................!</h4>
+        </div>
+    );
+};
+
+export default Home;
